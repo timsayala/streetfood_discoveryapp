b86151b3b3054848376510ac41e554883caa5c58
cyzanne <cyanranilmillam@gmail.com>
2023-03-15 19:18:29 +0800

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+      <div className="banner-content">
+        <div className="banner-image">
+        <img src="./images/Logo.jpg" className="Logo" alt="LOGO IMAGE"/>
+
+        </div>
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+        </nav>
+    )
+}
+export default Navbar;
