6837cae385eb2b215ce8c34fa2b66c8380635643
timsayala <timsayala1218@gmail.com>
2023-03-24 23:27:39 +0800

    Add some design

--- src/Components/pages/AboutUs.js
+++ src/Components/pages/AboutUs.js
@@ -9,8 +9,8 @@ const AboutUs = () => {
     
     <div>
       <Navbar style={{backgroundColor: '#f0f0f0'}}/>
-      <div className='container text-center'>
-        <div className='row d-flex align-items-center ' style={{backgroundColor: '#ffefe9'}} >
+      <div className='container-fluid text-center'>
+        <div className='row d-flex align-items-center ' style={{backgroundColor: '#dfc8a5'}} >
       <h3 className='col'>Discover the Best Streetfood in Your City!
       <p >Are you a foodie looking to explore the vibrant world of streetfood? Or maybe you're just tired of the same old restaurant options and want to try something new and exciting. Either way, you've come to the right place!</p></h3>
       <div className='col'>
@@ -25,7 +25,7 @@ const AboutUs = () => {
       <p >Our story began with a passion for street food. Our founder, John, was traveling abroad when he discovered the delicious and diverse world of street food. When he returned home, he realized that there was a lack of access to authentic and quality street food in his own city. That's when he decided to start Streetfood Discovery, a platform that connects foodies with the best street food vendors in town.</p></h3>
       
       </div>
-      <div className='row d-flex align-items-center '  style={{backgroundColor: '#ffefe9'}}>
+      <div className='row d-flex align-items-center '  style={{backgroundColor: '#dfc8a5'}}>
        <h3 className='col'>Our Mission
       <p >Our mission is to bring the taste and experience of street food to as many people as possible. We believe that everyone should have access to delicious and authentic street food, regardless of where they live. That's why we work hard to identify and promote the best street food vendors, and to make it easy for our users to discover and enjoy their offerings.</p></h3>
       
@@ -42,7 +42,7 @@ const AboutUs = () => {
       <p >At Streetfood Discovery, we value authenticity, quality, and community. We believe that street food is not just about the food itself, but also about the culture and community that surrounds it. That's why we strive to promote and support street food vendors who share these values, and to create a community of foodies who are passionate about street food.</p></h3>
       
       </div>
-      <div className='row d-flex align-items-center' style={{backgroundColor: '#ffefe9'}}>
+      <div className='row d-flex align-items-center' style={{backgroundColor: '#dfc8a5'}}>
        <h3 className='col'>Our Team
       <p >Our team is made up of dedicated foodies who are passionate about street food. From our developers to our customer service representatives, we all share a love for discovering and promoting the best street food around. We work hard to ensure that our platform is easy to use and that our users have a great experience discovering and enjoying street food.</p></h3>
       
--- src/Components/pages/Contact.js
+++ src/Components/pages/Contact.js
@@ -11,21 +11,34 @@ const Contact = () => {
       <div className='container contact '>
         
         
-      <h1 className='col-6'>GET IN TOUCH</h1>
+
+      <div className='row'> 
+
+      <div className='col-6 text-center'>
+      <h3>GET IN TOUCH</h3>
       
       
-      <p className='col-6'>If you have any questions, feedback, or just want to say hi, we'd love to hear from you! You can reach us at our official Facebook Page @stfooddevapp. We're always happy to chat about street food and how we can make the Streetfood Discovery experience even better for our users.</p>
+      <p>If you have any questions, feedback, or just want to say hi, we'd love to hear from you! You can reach us at our official Facebook Page @stfooddevapp. We're always happy to chat about street food and how we can make the Streetfood Discovery experience even better for our users.</p>
       <br></br>
-      <h3 className='col-6'> CHART TO US</h3>
-      <p className='col-6'>Our friendly team is here to help.</p>
-      <a className='col-6' href=''>streetfoodph@gmail.com</a>
-      <h3 className='col-6'>OFFICE</h3>
-      <p className='col-6'>Come say hello at our office</p>
-      <p className='col-6'>121 Rock Street,21 Avenue</p>
-      <h3 className='col-6'>PHONE</h3>
-      <p className='col-6'>Mon-Fri from 8am to 5pm</p>
-      <a className='col-6' href=''> +639613288009</a>
+      <h3> CHART TO US</h3>
+      <p>Our friendly team is here to help.</p>
+      <a>streetfoodph@gmail.com</a>
+      <h3>OFFICE</h3>
+      <p>Come say hello at our office</p>
+      <p>121 Rock Street,21 Avenue</p>
+      <h3>PHONE</h3>
+      <p>Mon-Fri from 8am to 5pm</p>
+      <a> +639613288009</a>
       </div>
+
+      <div className='col-6'>
+        <img  className="mx-auto d-block" src='../Images/cu.png' style={{width:
+        "330px", height: "600px" }}></img>
+      </div>
+      
+      </div>
+      </div>
+     
 
     <br></br>
     <br></br>
